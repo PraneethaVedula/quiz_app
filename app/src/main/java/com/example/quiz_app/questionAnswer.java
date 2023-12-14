64861812f9f9e6f97a222208a6966e3a064cb9c7
PraneethaVedula <praneethavedula@gmail.com>
2023-12-14 23:19:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/quiz_app/questionAnswer.java
@@ -0,0 +1,43 @@
+package com.example.quiz_app;
+
+public class questionAnswer {
+
+    public static String[] Questions = {
+            "Which country is the largest by land area?",
+            "Who painted the Mona Lisa?",
+            "What is the tallest mountain in the world?",
+            "What is the largest mammal in the world?",
+            "What year did World War II end?",
+            "What is the smallest prime number?",
+            "Which gas do plants absorb for photosynthesis?",
+            "What is the chemical symbol for gold?",
+            "Who wrote the play 'Hamlet'?",
+            "What is the smallest bone in the human body?"
+    };
+
+    public static String[][] choices = {
+            { "United States", "Russia", "Canada", "China"},
+            {"Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", "Michelangelo"},
+            {"Makalu", "K2", "Kangchenjunga", "Mount Everest"},
+            {"Blue whale","Giraffe", "African elephant",  "Polar bear"},
+            {"1939", "1940", "1942","1945"},
+            {"1", "2", "3", "5"},
+            {"Oxygen", "Carbon dioxide", "Nitrogen", "Carbon monoxide"},
+            {"Cu", "Ag", "Fe", "Au"},
+            {"Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"},
+            {"Femur", "Patella", "Stapes", "Cranium"}
+    };
+
+    public static String[] CorrectAnswer = {
+            "Russia",
+            "Leonardo da Vinci",
+            "Mount Everest",
+            "Blue whale",
+            "1945",
+            "2",
+            "Carbon dioxide",
+            "Au",
+            "William Shakespeare",
+            "Stapes"
+    };
+}
